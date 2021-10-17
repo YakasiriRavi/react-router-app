c9c45b23062c181077af200dab0803a629f3d234
YakasiriRavi <raviyakasiri77@gmail.com>
2021-10-17 16:08:17 +0530

    updated About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from 'react';
+import './Aboutstyle.css';
 const About = () => {
     return (
-        <div className="container">
-            <h1 className="text-center" style={{paddingTop:"30%"}}> About</h1>
+        <div className="co-container">
+            <center>
+            <h1 className="text-center" style={{paddingTop:"0%"}}> 
+                About ACE
+            </h1> </center>
+            <div className="row">
+            <div className="col-md 5 justify-content-center ms-3">
+            <img src="/images/acecmd.png" alt="primary" height="500px"/>
+            <h4>Prof. YV.Gopala Krishna Murthy </h4>
+            </div>
+            <div className="col-md-6">
+            <p className="">
+            ACE Engineering Academy was established in the year 1995 with a prime motto of imparting quality education in engineering and moulding the engineering students to crack competitive examinations. From a small coaching centre with just a handful of students in the year 1995, the institute has grown by leaps and bounds and established itself as one of the leading institutions in India for IES/GATE/PSUs coaching. The institute with its headquarters at Hyderabad draws students every year from all over the country. ACE has become the abode for engineering students who aspire to get into IISc/IITs after B.Tech and for those who cherish the dream of joining prestigious jobs through IES/PSUs.
+            </p>
+            <p>Prof YV Gopala Krishna Murthy graduated from University College of Engineering, Osmania University, Hyderabad. He holds Masters Degree (M.Tech) from NIT, Warangal, Selected for UPSC Engineering Services. One of the toppers in Engineering Services of AP Public Service Commission. With the interest in the teaching profession, he established ACE Engineering Academy in the year 1995 at Hyderabad. Guided thousands of engineering graduates for various competitive exams, and is instrumental in producing top rankers.
+
+He motivated thousands of students with his lectures on personality development. His passion for teaching and with his team of experts in various engineering specialisations, he has driven the Academy to new heights.
+Qualifications / Achievements of CMD, ACE Engineering Academy.</p>
+</div>
+</div>
             
         </div>
     )
