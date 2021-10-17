9b2cb07d96f1a2c77a2a31cba80cee830f1cc59d
B S Praveen <bsp.civil@gmail.com>
2021-10-17 16:33:54 +0530

    contact page updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="container">
-            <h1 className="text-center" style={{paddingTop:"30%"}}> Contact</h1>
+            <h1 className="text-center" style={{paddingTop:"0%"}}> Contact</h1>
+            <h2>Contacts has not been updated yet</h2>
             
         </div>
     )
